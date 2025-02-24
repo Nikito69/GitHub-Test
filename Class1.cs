bc1633844b9c2733c3434f1245e9ecd42f92da1b
MEGA <MEGA@gmail.com>
2025-02-24 12:19:32 +0200

    com

--- Class1.cs
+++ Class1.cs
@@ -8,6 +8,14 @@ public class ClassC
 	{
 		return Math.Sqrt(a);
 	}
-	
+	public double BabylonSqrt(double number)
+	{
+		dobule guess = number / 2;
+		while(Math.Abs(guess * guess - number) < 0.01)
+		{
+			double other = number / guess;
+			guess = (guess + other) / 2;
+		}
+		return guess;
 	}
 }
